51df858ccacfac2547d129027da2b2c2fe3759bb
Kauã Santos <kaua.ha.santos0100@gmail.com>
2026-02-26 21:21:45 -0300

    Java no VS Code e variavel

--- Aulas/src/Aula_01/Intro_java.java
+++ Aulas/src/Aula_01/Intro_java.java
@@ -4,6 +4,17 @@ public class Intro_java {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
+		int n1,n2, n3;
+
+		n1 = 10;
+		n2 = 10;
+
+		n3 = n1 + n2;
+
+		System.out.println(n3);
+
+
 	}
 
 }
